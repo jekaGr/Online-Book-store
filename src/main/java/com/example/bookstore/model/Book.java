013bed8516b6e7ffdee021b407d018e6b75c6b1b
jekaGr <jekagrusha@gmail.com>
2025-01-04 16:54:47 +0200

    create book store

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,34 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String author;
+    @NonNull
+    @Column(unique = true)
+    private String isbn;
+    @NonNull
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
+
---
+++ src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
+
