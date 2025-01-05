da1ed3dc0dc7999c85f0ce7ecbe80e2486af9a65
jekaGr <jekagrusha@gmail.com>
2025-01-05 16:17:58 +0200

    create create book store

---
+++ src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.exception.DataProcessingException;
+import com.example.bookstore.model.Book;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add book :" + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                    "SELECT u from Book u", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find all books", e);
+        }
+    }
+}
