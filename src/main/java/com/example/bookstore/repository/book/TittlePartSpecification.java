bf48bf7155268e507e5009596ba548e61d93490a
jekaGr <jekagrusha@gmail.com>
2025-01-23 15:00:44 +0200

    merged paginatiot

---
+++ src/main/java/com/example/bookstore/repository/book/TittlePartSpecification.java
@@ -0,0 +1,27 @@
+package com.example.bookstore.repository.book;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.SpecificationProvider;
+import jakarta.persistence.criteria.Predicate;
+import java.util.Arrays;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TittlePartSpecification implements SpecificationProvider<Book> {
+    public static final String TITLE = "title";
+
+    @Override
+    public String getKey() {
+        return TITLE;
+    }
+
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.or(
+                        Arrays.stream(params).map(param ->
+                                        criteriaBuilder.like(root.get(TITLE), "%" + param + "%"))
+                                .toArray(Predicate[]::new)
+                );
+    }
+}
---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,22 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.dto.BookDto;
+import com.example.bookstore.dto.BookSearchParameters;
+import com.example.bookstore.dto.CreateBookRequestDto;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BookService {
+    BookDto save(CreateBookRequestDto requestDto);
+
+    Page<BookDto> findAll(Pageable pageable);
+
+    BookDto findById(long id);
+
+    void deleteById(Long id);
+
+    BookDto updateById(Long id, CreateBookRequestDto createBookRequestDto);
+
+    public List<BookDto> search(BookSearchParameters bookSearchParameters);
+}
